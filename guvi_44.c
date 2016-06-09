1d9ff468222b3c32fdb83388ad808297342b1c01
azhagusurya-m <azhagusuryaa@gmail.com>
2016-06-09 21:21:45 +0530

    Create guvi_44.c

---
+++ guvi_44.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+         int input;    
+    	 scanf("%d",&input);
+	       if(input<=9&&input>=1)
+	             printf("entered value with in the range ,no error");
+	    	  else
+                    printf("you entered number is not range");
+	
+	return 0;
+}
