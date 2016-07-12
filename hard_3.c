be8a938079ed550e31f34d1f44531260647d08ff
azhagusurya-m <azhagusuryaa@gmail.com>
2016-07-12 09:53:41 +0530

    Update hard_3.c

--- hard_3.c
+++ hard_3.c
@@ -3,8 +3,10 @@
 int main()
 {
     int n,i;
+    printf("enter the number of array");
     scanf("%d",&n);
   int a[n];
+  printf("enter the elements in the array");
   for(i=0;i<n;i++)
     scanf("%d",&a[i]);
   for(i=0;i<n;i++)
