262e7dbe4dd9b499912dd790035181fb513412c1
azhagusurya-m <azhagusuryaa@gmail.com>
2016-07-12 09:55:50 +0530

    Update hard_3.c

--- hard_3.c
+++ hard_3.c
@@ -5,16 +5,20 @@ int main()
     int n,i;
     printf("enter the number of array");
     scanf("%d",&n);
-  int a[n];
-  printf("enter the elements in the array");
-  for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-  for(i=0;i<n;i++)
-  {
-        if(i==a[i])
-          printf("%d equals its  index %d\n",a[i],i);
-  }
-  
+    if(n<=0)
+     printf("0");
+     else
+     {
+          int a[n];
+          printf("enter the elements in the array");
+          for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+              for(i=0;i<n;i++)
+              {
+                    if(i==a[i])
+                      printf("%d equals its  index %d\n",a[i],i);
+              }
+     }
   
     return 0;
 }
