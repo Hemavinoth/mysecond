1e3e57aebb18072d7abe97ffb4c59ffc3db94395
azhagusurya-m <azhagusuryaa@gmail.com>
2016-07-12 22:15:16 +0530

    Create 01_matrix.c

---
+++ 01_matrix.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int a[n][n],b[n][n],i,j,k,flag=0;
+	printf("enter 0 and 1s ");
+	  for(i=0;i<n;i++)
+	  {
+	  	 for(j=0;j<n;j++)
+	  	  {
+	  	  
+	  	     	scanf("%d",&a[i][j]);
+	  	  }
+	  }
+	  
+	  for(i=0;i<n;i++)
+	  {
+	  	 for(j=0;j<n;j++)
+	  	  {
+	  	
+	  	if(a[i][j]==0)
+	  	  	{ 
+	  	    	
+	  	  	     for(k=0;k<n;k++)
+	  	  	     {
+	  	  	     	 b[i][k]=0;
+	  	  	     	 	
+	  	  	     	 b[k][j]=0;
+	  	  	     
+	  	  	     	 } 
+	  	  	     break;
+	  	  	     }
+	  	  	else
+	  	  	b[i][j]=a[i][j];
+	  	  	  	  }
+	  	  	  	  
+	  }  
+	
+	for(i=0;i<n;i++)
+	{
+		printf("\n");
+		for(j=0;j<n;j++)
+		{
+			printf("%d ",b[i][j]);
+		}
+		
+	}
+	
+	
+	return 0;
+}
