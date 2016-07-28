f5cc16fbc58f77dfd0af01faef471a8fd07cdabb
azhagusurya-m <azhagusuryaa@gmail.com>
2016-07-28 22:42:10 +0530

    Update prime_count.c

--- prime_count.c
+++ prime_count.c
@@ -3,6 +3,10 @@ int main(void) {
 	int num1,num2,count=0;
 	int i,flag=0,j;
 	scanf("%d %d",&num1,&num2);
+	if( num1 <0 &&num2<0)
+	printf("0");
+	else
+	{
 	for(j=num1;j<num2;j++)
 	{
 	for(i=2;i<=j/2;i++)
@@ -19,5 +23,6 @@ int main(void) {
 	   	
 	}
 	printf("%d",count);
+	}
 		return 0;
 }
