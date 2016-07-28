ef6e51d0e1ae9323c42819bea47f748299c06961
azhagusurya-m <azhagusuryaa@gmail.com>
2016-07-28 22:33:56 +0530

    Create prime_count.c

---
+++ prime_count.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+	int num1,num2,count=0;
+	int i,flag=0,j;
+	scanf("%d %d",&num1,&num2);
+	for(j=num1;j<num2;j++)
+	{
+	for(i=2;i<=j/2;i++)
+	{
+		if(j%i==0)
+		{
+			 flag=1;
+			 break;
+		}
+	}
+	if(flag!=1)
+	   count++;
+	   	flag=0;
+	   	
+	}
+	printf("%d",count);
+		return 0;
+}
